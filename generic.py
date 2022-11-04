6cebf6e9451fd036cc6e2fe63b7bb1de17f6bb6b
Ben Buzard <250144benjamin@gmail.com>
2022-11-04 10:05:44 -0700

    Add various things

--- generic.py
+++ generic.py
@@ -12,8 +12,19 @@ if "Ubuntu" in uname.version or "Debian" in uname.version:
     
     # update system
 
-    os.system('apt-get update')
-    os.system('apt-get upgrade')
+    os.system('apt-get update -y')
+    os.system('apt-get upgrade -y')
+
+    # install openscap dependencies
+
+    os.system("apt install libopenscap8 -y")
+    os.system("wget https://people.canonical.com/~ubuntu-security/oval/com.ubuntu.xenial.cve.oval.xml")
+
+    # install unattended upgrades
+
+    os.system("apt install unattended-upgrades -y")
+    os.system("apt install update-notifier-common -y")
+
 
     # get users
 
@@ -74,13 +85,33 @@ if "Ubuntu" in uname.version or "Debian" in uname.version:
 
     for user in allowed_users:
         if user == useracc: continue
+        print("Username: " + user)
         os.system("passwd " + user)
 
-    os.system("apt install libopenscap8")
-    os.system("wget https://people.canonical.com/~ubuntu-security/oval/com.ubuntu.xenial.cve.oval.xml")
+    # run openscap
+
+    print("running openscap")
     os.system("oscap oval eval --results oscap_results.xml --report oscap_report.html com.ubuntu.xenial.cve.oval.xml")
 
+    # enable firewall
+    print("firewall")
+    os.system("ufw enable")
+
+    # do last
     os.system("firefox oscap_report.html &")
+
+    ports_to_block = "20, 21, 23, 69, 135, 411, 412, 1080, 1194, 2302, 2745, 3074, 3124, 3127, 3128, 8080, 3306, 3724, 3784, 3785, 4333, 4444, 4664, 5004, 5005, 5500, 5554, 5800, 5900, 6112, 6500, 6699, 6881, 6882, 6883, 6884, 6885, 6886, 6887, 6888, 6889, 6890, 6891, 6892, 6893, 6894, 6895, 6896, 6897, 6898, 6999, 8767, 8866, 9898, 9988, 12035, 12036, 12345, 14567, 27015, 27374, 28960, 31337, 33434".split(", ")
+
+    for port in ports_to_block:
+        os.system("ufw deny " + port)
+
+    # this won't work in 20.04
+    # disable root login ssh
+    os.system('sed "s/PermitRootLogin.*/PermitRootLogin no/g" /etc/ssh/ssh_config > /etc/ssh/ssh_config')
+
+    # enable unattended upgrades
+    os.system('sed "s/Unattended-Upgrade::Automatic-Reboot \"false\"/Unattended-Upgrade::Automatic-Reboot \"true\"/g" /etc/apt/apt.conf.d/50unattended-upgrades > /etc/apt/apt.conf.d/50unattended-upgrades')
+    os.system('sed "s/APT::Periodic::Unattended-Upgrade \"0\";/APT::Periodic::Unattended-Upgrade \"1\";" /etc/apt/apt.conf.d/20auto-upgrades > /etc/apt/apt.conf.d/20auto-upgrades')
 
 elif "Darwin" in uname.version:
     print("Darwin not supported!")
