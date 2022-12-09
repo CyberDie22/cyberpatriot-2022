1a6bb6f1510af8541b90478c309c73814549a928
Ben Buzard <250144benjamin@gmail.com>
2022-12-08 21:43:47 -0800

    Add various things

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-import subprocess, platform
+import subprocess, platform, os
 
 def run(command):
     return subprocess.getoutput(command)
 
 uname = platform.uname()
 
-if uname.system == 'Linux':
-    if "Ubuntu" in uname.version:
-        lsb_release = run('lsb_release -r').split(':')[1].strip()
-        print(lsb_release)
+if "Ubuntu" in uname.version:
+    if os.geteuid() != 0:
+        print("This script requires root privliges!")
+        exit(1)
+
+    lsb_release = run('lsb_release -r').split(':')[1].strip()
+    if lsb_release == "22.04":
+        print("Running on Ubuntu Linux 22.04")
+
+        print("\nUpdate System")
+        run('apt update -y')
+        run('apt upgrade -y')
+
+        print("\nUpdate Users")
+        UID_MIN = int(run("awk '/^UID_MIN/ {print $2}' /etc/login.defs"))
+        UID_MAX = int(run("awk '/^UID_MAX/ {print $2}' /etc/login.defs"))
+        ME = run("logname")
+
+        print(ME)
+
+
+else:
+    print("Your system is not supported!")
+    exit(0)
