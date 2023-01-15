76bece2f073438b852b2e4d85468f51932e98164
Ben Buzard <250144benjamin@gmail.com>
2023-01-15 14:05:25 -0800

    Add more output to Ubuntu Start

--- scripts/ubuntu/start.sh
+++ scripts/ubuntu/start.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-apt update -y
-apt install python3-pip -y
+apt update -y >/dev/null
+
+echo "Installing python3-pip"
+apt install python3-pip -y >/dev/null
+echo "Installed python3-pip"
+
+echo "Installing tqdm from pip"
 python3 -m pip install tqdm
+echo "Installed tqdm from pip"
+
 python3 main.py
