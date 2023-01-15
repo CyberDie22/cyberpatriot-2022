3f711f0360109c0eab5de86ace6d82dc2aa9064a
Ben Buzard <250144benjamin@gmail.com>
2023-01-15 14:12:39 -0800

    try fix

--- scripts/ubuntu/main.py
+++ scripts/ubuntu/main.py
@@ -11,6 +11,6 @@ def run_with_output(command: [str]) -> str:
     return subprocess.check_output(command).decode()
 
 
-release = run_with_output(["lsb_release", "-r"])
+release = run_with_output(["lsb_release", "-r"]).split("\t")[-1]
 
 print(release)
