e33ed8f3e174b817dc38df513b59ac2a0e88d91f
Ben Buzard <250144benjamin@gmail.com>
2023-01-15 14:08:44 -0800

    Get release in Ubuntu Main

--- scripts/ubuntu/main.py
+++ scripts/ubuntu/main.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 
-a = input("hey")
-print(a)
+import platform, subprocess, os
+
+
+def run(command: [str]) -> int:
+    return subprocess.check_call(command)
+
+
+def run_with_output(command: [str]) -> str:
+    return subprocess.check_output(command)
+
+
+release = run_with_output(["lsb_release", "-r"]).split(" ")[-1]
+
+print(release)
