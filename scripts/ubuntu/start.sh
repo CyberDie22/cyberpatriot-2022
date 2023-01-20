06254aebecfee4b119d69af281eac4b37da8e017
Ben Buzard <250144benjamin@gmail.com>
2023-01-19 21:20:28 -0800

    Update ubuntu start script

--- scripts/ubuntu/start.sh
+++ scripts/ubuntu/start.sh
@@ -6,11 +6,11 @@ apt install python3-pip -y >/dev/null 2>/dev/null
 echo "Installed python3-pip"
 
 echo "Installing tqdm from pip"
-python3 -m pip install tqdm
+python3 -m pip install tqdm >/dev/null 2>/dev/null
 echo "Installed tqdm from pip"
 
 echo "Installing python-apt from pip"
-python3 -m pip install python-apt
+python3 -m pip install python-apt >/dev/null 2>/dev/null
 echo "Installed python-apt from pip"
 
 python3 main.py
